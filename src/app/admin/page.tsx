563e0e53b1c1bd6366ff8c6210c62db2d47ea9a5
dsmarcelo <celoygm@gmail.com>
2024-08-01 19:11:13 -0300

    chore: fix layout classnames

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ValidateVoucher from '../_components/validate-voucher'
-import PasswordLoginForm from '../_components/passwordLoginForm'
-import { isLoggedIn, logout } from '../lib'
 
 export default async function AdminPage() {
   return (
-    <main className='flex min-h-screen w-full flex-col items-center px-4'>
-      <div className='mt-12'>
+    <main className='flex w-full flex-col items-center px-4'>
+      <div className='mt-8'>
         <ValidateVoucher />
       </div>
     </main >
