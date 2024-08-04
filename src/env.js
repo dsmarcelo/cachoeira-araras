49ee90b53bdda6639819fefff65827fe87a3442c
dsmarcelo <celoygm@gmail.com>
2024-08-04 13:55:26 -0300

    build: add env checks in env.js

--- src/env.js
+++ src/env.js
@@ -21,6 +21,11 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url()
     ),
+    DATABASE_URL: z.string(),
+    URL: z.string(),
+    MERCADOPAGO_TOKEN: z.string(),
+    WEBHOOK_URL: z.string(),
+    CRON_SECRET: z.string(),
   },
 
   /**
@@ -40,6 +45,11 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    URL: process.env.URL,
+    MERCADOPAGO_TOKEN: process.env.MERCADOPAGO_TOKEN,
+    WEBHOOK_URL: process.env.WEBHOOK_URL,
+    CRON_SECRET: process.env.CRON_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
