e217527ea2766fb204fb74f4591e697b28dda29a
dsmarcelo <celoygm@gmail.com>
2024-08-05 20:02:03 -0300

    feat: start implementing new image voucher card

--- src/app/_components/voucher-card.tsx
+++ src/app/_components/voucher-card.tsx
@@ -23,18 +23,31 @@ export default function VoucherCard({ data }: { data: Voucher }) {
     }
   }
 
+  const { name, phone, adults, elderly, status, expires_at, code } = data;
+
+  // Crie a string de query parameters
+  // const queryParams = `?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&adults=${adults}&elderly=${elderly}&status=${status}&code=${code}`;
+  const queryParams = `?name=${phone}`;
+  // const imgURL = `http://localhost:3000/api/og${queryParams}`
+  // const imgURL = `http://localhost:3000/api/og?name=${encodeURIComponent(name)}&phone=${phone}&adults=${adults}&elderly=${elderly}&status=${status}&code=${code}`
+  const imgURL = `http://localhost:3000/api/og?name=Maria%20Aparecida%20Silva&phone=619999999999&adults=1&elderly=0&status=valid&code=f22m`
+
+  console.log('🚀 ~ VoucherCard ~ imgURL:', imgURL);
   return (
-    <div className='w-[400px] relative max-w-3xl aspect-[2/1] text-bg-blue mx-auto bg-[url(/voucher_card.png)] bg-cover scale-75 min-[375px]:scale-90 min-[428px]:scale-100 -translate-y-8 sm:scale-125 sm:translate-y-[20px] md:scale-150 md:translate-y-1/4'>
-      <div className='relative top-12 left-4 flex flex-col gap-1 font-semibold'>
-        <p>{truncateName(data.name)}</p>
-        <p>{formatPhone(data.phone)}</p>
-        <p>{formatQuantity({ adults: data.adults, elderly: data.elderly })}</p>
-        {data.status !== 'valid' ? <p className='w-fit'>{formatVoucherStatus(data.status)}</p>
-          : <p>Valido até: {data.expires_at ? formateDateDayMonthYear(data.expires_at) : 'Não informado'}</p>
-        }
-      </div>
-      {/* <h3 className='relative -bottom-[42px] left-[144px] text-2xl font-bold text-center'>{data.code}</h3> */}
-      <h3 className='absolute w-fit bottom-0 right-0 mr-[26px] mb-[18px] text-2xl font-bold text-center'>{data.code}</h3>
+    // <div className='w-[400px] relative max-w-3xl aspect-[2/1] text-bg-blue mx-auto bg-[url(/voucher_card.png)] bg-cover scale-75 min-[375px]:scale-90 min-[428px]:scale-100 -translate-y-8 sm:scale-125 sm:translate-y-[20px] md:scale-150 md:translate-y-1/4'>
+    //   <div className='relative top-12 left-4 flex flex-col gap-1 font-semibold'>
+    //     <p>{truncateName(data.name)}</p>
+    //     <p>{formatPhone(data.phone)}</p>
+    //     <p>{formatQuantity({ adults: data.adults, elderly: data.elderly })}</p>
+    //     {data.status !== 'valid' ? <p className='w-fit'>{formatVoucherStatus(data.status)}</p>
+    //       : <p>Valido até: {data.expires_at ? formateDateDayMonthYear(data.expires_at) : 'Não informado'}</p>
+    //     }
+    //   </div>
+    //   {/* <h3 className='relative -bottom-[42px] left-[144px] text-2xl font-bold text-center'>{data.code}</h3> */}
+    //   <h3 className='absolute w-fit bottom-0 right-0 mr-[26px] mb-[18px] text-2xl font-bold text-center'>{data.code}</h3>
+    // </div>
+    <div>
+      <Image src={imgURL} width={400} height={400} alt='Voucher' />
     </div>
   )
 }
---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,131 @@
+import { ImageResponse } from 'next/og';
+import { formateDateDayMonthYear, truncateName } from '@/lib/utils'
+import { formatPhone } from '@/lib/utils'
+import { type Voucher } from '@prisma/client'
+import { type NextRequest } from 'next/server';
+// App router includes @vercel/og.
+// No need to install it.
+export const runtime = 'edge';
+
+export function formatVoucherStatus(status: string, expiration_date: string) {
+  if (!status) return <p className="text-red-500">Voucher inválido</p>;
+  switch (status) {
+    case "pending":
+      return <span className="text-yellow-700 w-fit bg-yellow-200/30 rounded-lg px-1 pb-1">Aguardando pagamento</span>
+    case "valid":
+      return <span style={{
+        color: '#10b981',
+        backgroundColor: 'rgba(199, 232, 204, 0.3)',
+        borderRadius: '0.375rem',
+        padding: '0 1px 1px'
+      }}>Valido até: {expiration_date}</span>;
+    case "redeemed":
+      return <span className="text-red-500 w-fit bg-red-200/30 rounded-lg px-1 pb-1">Voucher já resgatado</span>;
+    case "expired":
+      return <span className="text-slate-500 w-fit bg-slate-200/30 rounded-lg px-1 pb-1">Voucher expirado</span>;
+    default:
+      return <span className="text-red-500 w-fit bg-red-200/30 rounded-lg px-1 pb-1">Voucher inválido</span>;
+  }
+}
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  console.log('🚀 ~ GET ~ request.nextUrl:', searchParams.get('name'));
+  // const queries = Object.fromEntries(searchParams.entries());
+  const { name, phone, adults, elderly, status, expires_at, code } = Object.fromEntries(searchParams.entries());
+  if (!name || !phone || !adults || !elderly || !status || !code) return new Response('Voucher not found', { status: 404 });
+  // const { name } = Object.fromEntries(searchParams.entries());
+  function formatQuantity({ adults, elderly }: { adults: number; elderly: number; }): string {
+    const adultsText = adults === 1 ? '1 inteira' : `${adults} inteiras`;
+    const elderlyText = elderly === 1 ? '1 meia' : `${elderly} meias`;
+
+    if (adults > 0 && elderly > 0) {
+      return `${adultsText} e ${elderlyText}`;
+    } else if (adults > 0) {
+      return adultsText;
+    } else if (elderly > 0) {
+      return elderlyText;
+    } else {
+      return 'Nenhuma entrada';
+    }
+  }
+
+  const formatedName = truncateName(name);
+  const formatedPhone = formatPhone(phone);
+  const formatedQuantity = formatQuantity({ adults: parseInt(adults), elderly: parseInt(elderly) });
+  const expiration_date = expires_at ? formateDateDayMonthYear(expires_at) : 'Não informado';
+  console.log('🚀 ~ GET ~ expiration_date:', expiration_date);
+  const formatedStatus = formatVoucherStatus(status, expiration_date);
+
+  // const { name, phone, adults, elderly, status, expires_at, code } = searchParams;
+  // const { name } = searchParams;
+
+  //console.log the fullurl
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'black',
+          display: 'flex',
+          color: 'white',
+          backgroundImage: `url(http://localhost:3000/voucher_card.png)`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div tw='flex relative' style={{ color: 'black' }}>
+          <div tw='flex text-6xl absolute top-40 left-20 flex-col font-bold tracking-tight' style={{ fontStyle: 'italic' }}>
+            <span>{formatedName}</span>
+            <span>{formatedPhone}</span>
+            <span>{formatedQuantity}</span>
+            <span>{formatedStatus}</span>
+            {/* <span>{formatVoucherStatus(status)}</span> */}
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1500,
+      height: 750,
+    },
+  );
+}
+
+{/* <p>{formatQuantity({ adults: adults, elderly: elderly })}</p> */ }
+{/* {status !== 'valid' ? <p tw='w-fit'>{formatVoucherStatus(status)}</p> */ }
+{/* : <span>Valido até: {expires_at ? formateDateDayMonthYear(expires_at) : 'Não informado'}</span> */ }
+{/* } */ }
+
+{/* <h3 tw='relative -bottom-[42px] left-[144px] text-2xl font-bold text-center'>{code}</h3> */ }
+{/* <h3 tw='absolute w-fit bottom-0 right-0 mr-[26px] mb-[18px] text-2xl font-bold text-center'>{code}</h3> */ }
+
+{/* <div tw="bg-gray-50 flex">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+              <span>{name}</span>
+              <span tw="text-indigo-600">{phone}</span>
+            </h2>
+            <div tw="mt-8 flex md:mt-0">
+              <div tw="flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
+                >
+                  Get started
+                </a>
+              </div>
+              <div tw="ml-3 flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div> */}
