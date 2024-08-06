5d6bd9476272b70c439dc465924718744c44304e
dsmarcelo <celoygm@gmail.com>
2024-08-06 16:27:05 -0300

    feat: implement image voucher card using vercel-og

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,12 +1,22 @@
 import { ImageResponse } from 'next/og';
-import { formateDateDayMonthYear, truncateName } from '@/lib/utils'
+import { truncateName } from '@/lib/utils'
 import { formatPhone } from '@/lib/utils'
-import { type Voucher } from '@prisma/client'
 import { type NextRequest } from 'next/server';
 
-// App router includes @vercel/og.
-// No need to install it.
 export const runtime = 'edge';
+
+const interMedium = fetch(
+  new URL("../../../../assets/fonts/Inter-Medium.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+const interRegular = fetch(
+  new URL("../../../../assets/fonts/Inter-Regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+const interBold = fetch(
+  new URL("../../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+const interSemiBold = fetch(
+  new URL("../../../../assets/fonts/Inter-SemiBold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
 
 export function formatVoucherStatus(status: string, expiration_date: string) {
   if (!status) return <p className="text-red-500">Voucher inválido</p>;
@@ -16,9 +26,9 @@ export function formatVoucherStatus(status: string, expiration_date: string) {
     case "valid":
       return <span style={{
         color: '#10b981',
-        backgroundColor: 'rgba(199, 232, 204, 0.3)',
+        // backgroundColor: 'rgba(199, 232, 204, 0.3)',
         borderRadius: '0.375rem',
-        padding: '0 1px 1px'
+        // padding: '0 1px 1px'
       }}>Valido até: {expiration_date}</span>;
     case "redeemed":
       return <span className="text-red-500 w-fit bg-red-200/30 rounded-lg px-1 pb-1">Voucher já resgatado</span>;
@@ -31,11 +41,16 @@ export function formatVoucherStatus(status: string, expiration_date: string) {
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  console.log('🚀 ~ GET ~ request.nextUrl:', searchParams.get('name'));
-  // const queries = Object.fromEntries(searchParams.entries());
   const { name, phone, adults, elderly, status, expires_at, code } = Object.fromEntries(searchParams.entries());
-  if (!name || !phone || !adults || !elderly || !status || !code) return new Response('Voucher not found', { status: 404 });
-  // const { name } = Object.fromEntries(searchParams.entries());
+
+  if (!name || !phone || !adults || !elderly || !status || !code) {
+    return new Response('Voucher not found', { status: 404 });
+  }
+  const interMediumFontData = await interMedium;
+  const interRegularFontData = await interRegular;
+  const interBoldFontData = await interBold;
+  const interSemiBoldFontData = await interSemiBold;
+
   function formatQuantity({ adults, elderly }: { adults: number; elderly: number; }): string {
     const adultsText = adults === 1 ? '1 inteira' : `${adults} inteiras`;
     const elderlyText = elderly === 1 ? '1 meia' : `${elderly} meias`;
@@ -54,14 +69,8 @@ export async function GET(request: NextRequest) {
   const formatedName = truncateName(name);
   const formatedPhone = formatPhone(phone);
   const formatedQuantity = formatQuantity({ adults: parseInt(adults), elderly: parseInt(elderly) });
-  const expiration_date = expires_at ? formateDateDayMonthYear(expires_at) : '-';
-  console.log('🚀 ~ GET ~ expiration_date:', expiration_date);
+  const expiration_date = expires_at ? expires_at : '';
   const formatedStatus = formatVoucherStatus(status, expiration_date);
-
-  // const { name, phone, adults, elderly, status, expires_at, code } = searchParams;
-  // const { name } = searchParams;
-
-  //console.log the fullurl
 
   return new ImageResponse(
     (
@@ -73,7 +82,7 @@ export async function GET(request: NextRequest) {
           backgroundColor: 'black',
           display: 'flex',
           color: 'white',
-          backgroundImage: `url(http://localhost:3000/voucher_card.png)`,
+          backgroundImage: `url(http://localhost:3000/voucher_card.jpg)`,
           backgroundSize: 'contain',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
@@ -81,14 +90,14 @@ export async function GET(request: NextRequest) {
           fontWeight: 900,
         }}
       >
-        <div tw='flex font-bold' style={{ color: 'black', fontWeight: 900 }}>
-          <div tw='flex text-5xl absolute top-40 left-20 flex-col tracking-tight font-bold' style={{ gap: '4px' }}>
-            <div tw='flex' style={{ fontWeight: 900 }}>{formatedName}</div>
-            <div tw='flex font-bold'>{formatedPhone}</div>
-            <div tw='flex font-bold'>{formatedQuantity}</div>
-            <div tw='flex font-bold'>{formatedStatus}</div>
-            {/* <span>{formatVoucherStatus(status)}</span> */}
+        <div tw='flex relative' style={{ color: '#00182D' }}>
+          <div tw='flex text-[68px] absolute top-46 left-16 flex-col tracking-tight font-semibold' style={{ gap: '30px' }}>
+            <div tw='flex font-semibold'>{formatedName}</div>
+            <div tw='flex font-semibold'>{formatedPhone}</div>
+            <div tw='flex font-semibold'>{formatedQuantity}</div>
+            <div tw='flex font-semibold'>{formatedStatus}</div>
           </div>
+          <div tw='flex absolute text-[90px] font-bold bottom-16 left-[1175px]'>{code}</div>
         </div>
       </div>
     ),
@@ -96,41 +105,28 @@ export async function GET(request: NextRequest) {
       width: 1500,
       height: 750,
       status: 200,
+      fonts: [
+        {
+          name: "Inter",
+          data: interMediumFontData,
+          weight: 500,
+        },
+        {
+          name: "Inter",
+          data: interRegularFontData,
+          weight: 400,
+        },
+        {
+          name: "Inter",
+          data: interBoldFontData,
+          weight: 700,
+        },
+        {
+          name: "Inter",
+          data: interSemiBoldFontData,
+          weight: 600,
+        },
+      ],
     },
   );
 }
-
-{/* <p>{formatQuantity({ adults: adults, elderly: elderly })}</p> */ }
-{/* {status !== 'valid' ? <p tw='w-fit'>{formatVoucherStatus(status)}</p> */ }
-{/* : <span>Valido até: {expires_at ? formateDateDayMonthYear(expires_at) : 'Não informado'}</span> */ }
-{/* } */ }
-
-{/* <h3 tw='relative -bottom-[42px] left-[144px] text-2xl font-bold text-center'>{code}</h3> */ }
-{/* <h3 tw='absolute w-fit bottom-0 right-0 mr-[26px] mb-[18px] text-2xl font-bold text-center'>{code}</h3> */ }
-
-{/* <div tw="bg-gray-50 flex">
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
-              <span>{name}</span>
-              <span tw="text-indigo-600">{phone}</span>
-            </h2>
-            <div tw="mt-8 flex md:mt-0">
-              <div tw="flex rounded-md shadow">
-                <a
-                  href="#"
-                  tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
-                >
-                  Get started
-                </a>
-              </div>
-              <div tw="ml-3 flex rounded-md shadow">
-                <a
-                  href="#"
-                  tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
-                >
-                  Learn more
-                </a>
-              </div>
-            </div>
-          </div>
-        </div> */}
