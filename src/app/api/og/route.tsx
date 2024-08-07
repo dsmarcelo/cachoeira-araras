1b42eb4138975a96c407cad0585e5c7f183c5760
dsmarcelo <celoygm@gmail.com>
2024-08-07 13:20:24 -0300

    chore: reduce edge funcion size

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,42 +2,16 @@ import { ImageResponse } from 'next/og';
 import { truncateName } from '@/lib/utils'
 import { formatPhone } from '@/lib/utils'
 import { type NextRequest } from 'next/server';
+import { formatVoucherStatusWithoutBg } from '@/lib/voucher';
 
 export const runtime = 'edge';
 
-const interMedium = fetch(
-  new URL("../../../../assets/fonts/Inter-Medium.ttf", import.meta.url)
-).then((res) => res.arrayBuffer());
-const interRegular = fetch(
-  new URL("../../../../assets/fonts/Inter-Regular.ttf", import.meta.url)
-).then((res) => res.arrayBuffer());
 const interBold = fetch(
   new URL("../../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 const interSemiBold = fetch(
   new URL("../../../../assets/fonts/Inter-SemiBold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
-
-function formatVoucherStatus(status: string, expiration_date: string) {
-  if (!status) return <p className="text-red-500">Voucher inválido</p>;
-  switch (status) {
-    case "pending":
-      return <span className="text-yellow-700 w-fit bg-yellow-200/30 rounded-lg px-1 pb-1">Aguardando pagamento</span>
-    case "valid":
-      return <span style={{
-        color: '#10b981',
-        // backgroundColor: 'rgba(199, 232, 204, 0.3)',
-        borderRadius: '0.375rem',
-        // padding: '0 1px 1px'
-      }}>Valido até: {expiration_date}</span>;
-    case "redeemed":
-      return <span className="text-red-500 w-fit bg-red-200/30 rounded-lg px-1 pb-1">Voucher já resgatado</span>;
-    case "expired":
-      return <span className="text-slate-500 w-fit bg-slate-200/30 rounded-lg px-1 pb-1">Voucher expirado</span>;
-    default:
-      return <span className="text-red-500 w-fit bg-red-200/30 rounded-lg px-1 pb-1">Voucher inválido</span>;
-  }
-}
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -46,8 +20,7 @@ export async function GET(request: NextRequest) {
   if (!name || !phone || !adults || !elderly || !status || !code) {
     return new Response('Voucher not found', { status: 404 });
   }
-  const interMediumFontData = await interMedium;
-  const interRegularFontData = await interRegular;
+
   const interBoldFontData = await interBold;
   const interSemiBoldFontData = await interSemiBold;
 
@@ -70,7 +43,7 @@ export async function GET(request: NextRequest) {
   const formatedPhone = formatPhone(phone);
   const formatedQuantity = formatQuantity({ adults: parseInt(adults), elderly: parseInt(elderly) });
   const expiration_date = expires_at ? expires_at : '';
-  const formatedStatus = formatVoucherStatus(status, expiration_date);
+  const formatedStatus = formatVoucherStatusWithoutBg(status, expiration_date);
 
   return new ImageResponse(
     (
@@ -82,40 +55,30 @@ export async function GET(request: NextRequest) {
           backgroundColor: 'black',
           display: 'flex',
           color: 'white',
-          backgroundImage: `url(http://localhost:3000/voucher_card.jpg)`,
+          backgroundImage: `url(http://localhost:3000/voucher_card_w_750px.jpg)`,
           backgroundSize: 'contain',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
-          fontSize: 32,
+          fontSize: 18,
           fontWeight: 900,
         }}
       >
         <div tw='flex relative' style={{ color: '#00182D' }}>
-          <div tw='flex text-[68px] absolute top-46 left-16 flex-col tracking-tight font-semibold' style={{ gap: '30px' }}>
+          <div tw='flex text-[34px] absolute top-22 left-8 flex-col tracking-tight font-semibold' style={{ gap: '14px' }}>
             <div tw='flex font-semibold'>{formatedName}</div>
             <div tw='flex font-semibold'>{formatedPhone}</div>
             <div tw='flex font-semibold'>{formatedQuantity}</div>
             <div tw='flex font-semibold'>{formatedStatus}</div>
           </div>
-          <div tw='flex absolute text-[90px] font-bold bottom-16 left-[1175px]'>{code}</div>
+          <div tw='flex absolute text-[48px] font-bold bottom-8 left-[585px]'>{code}</div>
         </div>
       </div>
     ),
     {
-      width: 1500,
-      height: 750,
+      width: 750,
+      height: 375,
       status: 200,
       fonts: [
-        {
-          name: "Inter",
-          data: interMediumFontData,
-          weight: 500,
-        },
-        {
-          name: "Inter",
-          data: interRegularFontData,
-          weight: 400,
-        },
         {
           name: "Inter",
           data: interBoldFontData,
