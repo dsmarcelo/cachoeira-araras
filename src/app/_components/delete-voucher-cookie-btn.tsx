22ca382c81cef07ffb3d91ef0a940e05b2e77ddf
dsmarcelo <celoygm@gmail.com>
2024-08-12 22:19:32 -0300

    feat: add share button to /pagamento page and change styling

--- src/app/_components/delete-voucher-cookie-btn.tsx
+++ src/app/_components/delete-voucher-cookie-btn.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 import { deleteCookieVoucher } from '../lib'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export default function DeleteVoucherCookieBtn() {
   const router = useRouter()
@@ -12,6 +23,24 @@ export default function DeleteVoucherCookieBtn() {
   }
 
   return (
-    <Button onClick={resetCookieVoucher}>Comprar outro voucher</Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"ghost"} className='w-full text-light rounded-full py-0 h-8'>Comprar outro voucher</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Deseja deletar este voucher do seu navegador e comprar outro?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Seu voucher continuara a funcionar, mas não será mais exibido no seu navegador.
+            <p className='font-bold'>Tenha certeza que guardou o codigo do voucher antes de continuar.</p>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={resetCookieVoucher}>Continuar</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
+// <Button className='rounded-full'>Comprar outro voucher</Button>
