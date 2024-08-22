9eea7fd67b8103272fd946bc1bf0b0dbbf4a9fb5
dsmarcelo <celoygm@gmail.com>
2024-08-22 12:03:35 -0300

    style: change hover style in header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -12,25 +12,25 @@ export default function Header() {
   return (
     <header className="top-0 max flex h-16 md:h-24 items-center gap-4 border-b-primary-500 bg-dark-blue text-primary-400 px-4 md:px-6">
       <div className='flex items-center w-full mx-auto max-w-5xl'>
-        <nav className="hidden gap-4 text-lg font-medium md:flex md:flex-row md:items-center z-10">
+        <nav className="hidden gap-1 text-lg font-medium md:flex md:flex-row md:items-center z-10">
           <div className='w-full flex flex-col mr-8'>
             <Image src="/logo_nome.png" alt="logo" className='' width={120} height={80} />
           </div>
           <Link
             href="/"
-            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 rounded-lg hover:bg-slate-100"
+            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 px-3 rounded-lg hover:bg-bg-blue/20"
           >
             Inicio
           </Link>
           <Link
             href="/comprar"
-            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 rounded-lg hover:bg-slate-100"
+            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 px-3 rounded-lg hover:bg-bg-blue/20"
           >
             Comprar voucher
           </Link>
           <Link
             href="/galeria"
-            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 rounded-lg hover:bg-slate-100"
+            className="flex items-center whitespace-nowrap gap-2 font-semibold md:text-base p-2 px-3 rounded-lg hover:bg-bg-blue/20"
           >
             Galeria
           </Link>
@@ -39,7 +39,7 @@ export default function Header() {
           <SheetTrigger asChild>
             <Button
               size="icon"
-              className="shrink-0 bg-transparent text-current hover:bg-primary-700 md:hidden z-10"
+              className="shrink-0 bg-transparent text-current hover:bg-bg-blue/20 md:hidden z-10"
             >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Abrir menu</span>
@@ -69,21 +69,21 @@ export default function Header() {
           <Link
             href={"https://www.facebook.com/C.Araras/?locale=pt_BR"}
             target='_blank'
-            className='w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-primary-700'
+            className='w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-bg-blue/20'
           >
             <FaFacebook className='w-5 h-5' />
           </Link>
           <Link
             href={"https://www.instagram.com/cachoeiradasararasoficial/"}
             target='_blank'
-            className='w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-primary-700'
+            className='w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-bg-blue/20'
           >
             <FaInstagram className='w-5 h-5' />
           </Link>
           <Link
             href="https://wa.me/556299251040?"
             target='_blank'
-            className="w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-primary-700"
+            className="w-12 h-12 flex items-center justify-center text-foreground transition-colors p-2 rounded-lg hover:bg-bg-blue/20"
           >
             <FaWhatsapp className="w-5 h-5" />
           </Link>
