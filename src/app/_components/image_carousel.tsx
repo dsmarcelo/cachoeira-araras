0bb2e731f59e744bfd4344b20f1877b4bd9c5380
dsmarcelo <celoygm@gmail.com>
2024-08-23 08:40:30 -0300

    style: change carousel height with screen height and make it a link

--- src/app/_components/image_carousel.tsx
+++ src/app/_components/image_carousel.tsx
@@ -9,6 +9,7 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel"
 import Image from "next/image"
+import Link from "next/link"
 
 export function ImageCarousel() {
   const autoplay = React.useRef(
@@ -27,29 +28,31 @@ export function ImageCarousel() {
   }
 
   return (
-    <Carousel
-      plugins={[autoplay.current, fade.current]}
-      opts={{
-        loop: true,
-      }}
-      className="w-full aspect-[2/1] max-w-5xl mx-auto lg:rounded-2xl lg:mt-8 overflow-hidden"
-    >
-      <CarouselContent>
-        {getImages().map((image, index) => (
-          <CarouselItem key={index}>
-            <div className="w-full my-auto aspect-video object-center relative">
-              <Image
-                src={image}
-                alt="Imagem"
-                fill className="object-cover"
-                quality={100}
-                priority
-                sizes="(max-width: 768px) 100vw, 75vw"
-              />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <Link href="/galeria" className="w-full max-w-5xl mx-auto lg:rounded-2xl lg:mt-8 overflow-hidden">
+      <Carousel
+        plugins={[autoplay.current, fade.current]}
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {getImages().map((image, index) => (
+            <CarouselItem key={index} className="w-full aspect-[2/1] md:max-tall:aspect-[3/1]">
+              <div className="w-full h-full relative">
+                <Image
+                  src={image}
+                  alt="Imagem"
+                  fill
+                  className="object-cover"
+                  quality={85}
+                  priority
+                  sizes="(max-width: 768px) 100vw, 75vw"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </Link>
   )
 }
