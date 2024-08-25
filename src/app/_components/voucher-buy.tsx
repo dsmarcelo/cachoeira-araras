a0c5c5f9f51abb26bc3afdcbf371e68fb2433c54
dsmarcelo <celoygm@gmail.com>
2024-08-25 20:35:02 -0300

    style: animation in voucher-buy card

--- src/app/_components/voucher-buy.tsx
+++ src/app/_components/voucher-buy.tsx
@@ -1,12 +1,26 @@
+'use client'
 import React from 'react'
+import { motion } from 'framer-motion'
 import PriceTable from './price-table'
 import VoucherForm from './voucher-form'
 
 export default function VoucherBuy() {
   return (
-    <div className='mx-auto w-full max-w-2xl bg-dark-blue rounded-xl overflow-hidden'>
-      <PriceTable />
-      <VoucherForm />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+      viewport={{ margin: "-50px 0px -100px 0px" }}
+      className='mx-auto w-full max-w-2xl bg-dark-blue rounded-xl overflow-hidden'
+    >
+      <motion.div
+      >
+        <PriceTable />
+      </motion.div>
+      <motion.div
+      >
+        <VoucherForm />
+      </motion.div>
+    </motion.div>
   )
 }
