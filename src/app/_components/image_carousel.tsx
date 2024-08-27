e9430d4fa662b6e04db6b2a34957735c75bc530a
dsmarcelo <celoygm@gmail.com>
2024-08-27 17:05:08 -0300

    style: add animation to image carousel

--- src/app/_components/image_carousel.tsx
+++ src/app/_components/image_carousel.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/carousel"
 import Image from "next/image"
 import Link from "next/link"
+import { motion } from "framer-motion"
 
 export function ImageCarousel() {
   const autoplay = React.useRef(
@@ -28,31 +29,38 @@ export function ImageCarousel() {
   }
 
   return (
-    <Link href="/galeria" className="w-full max-w-5xl mx-auto lg:rounded-2xl lg:mt-8 overflow-hidden">
-      <Carousel
-        plugins={[autoplay.current, fade.current]}
-        opts={{
-          loop: true,
-        }}
-      >
-        <CarouselContent>
-          {getImages().map((image, index) => (
-            <CarouselItem key={index} className="w-full aspect-[2/1] md:max-tall:aspect-[3/1]">
-              <div className="w-full h-full relative">
-                <Image
-                  src={image}
-                  alt="Imagem"
-                  fill
-                  className="object-cover"
-                  quality={85}
-                  priority
-                  sizes="(max-width: 768px) 100vw, 75vw"
-                />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-    </Link>
+    <motion.div
+      initial={{ opacity: 0, y: -16 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full max-w-5xl mx-auto lg:rounded-2xl lg:mt-8 overflow-hidden"
+    >
+      <Link href="/galeria" className="w-full max-w-5xl mx-auto lg:rounded-2xl lg:mt-8 overflow-hidden">
+        <Carousel
+          plugins={[autoplay.current, fade.current]}
+          opts={{
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            {getImages().map((image, index) => (
+              <CarouselItem key={index} className="w-full aspect-[2/1] md:max-tall:aspect-[3/1]">
+                <div className="w-full h-full relative">
+                  <Image
+                    src={image}
+                    alt="Imagem"
+                    fill
+                    className="object-cover"
+                    quality={85}
+                    priority
+                    sizes="(max-width: 768px) 100vw, 75vw"
+                  />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </Link>
+    </motion.div>
   )
 }
