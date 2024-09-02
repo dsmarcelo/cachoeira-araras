c8a158007e61e0f079b01a3d8107c67f7c2a5861
dsmarcelo <celoygm@gmail.com>
2024-09-02 13:22:35 -0300

    chore: update deleteVoucherAlert component

--- src/app/_components/voucher-created-card.tsx
+++ src/app/_components/voucher-created-card.tsx
@@ -6,6 +6,7 @@ import { FaArrowLeft } from 'react-icons/fa'
 import { toast } from '@/components/ui/use-toast'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import DeleteVoucherCookieBtn from './delete-voucher-cookie-btn'
 
 export default function VoucherCreatedCard(
   { code, init_point, redirectToPayment, setCode, payment_success_url }:
@@ -47,21 +48,14 @@ export default function VoucherCreatedCard(
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <Button onClick={() => handleClick(true)} className='mb-4 h-8 bg-dark-blue text-primary-50'>
-        <FaArrowLeft className='mr-2 h-3 w-3' />
-        <p>Voltar</p>
-      </Button>
       <div className='flex flex-col gap-6'>
         {payment_success_url ? (
           <div className='p-4'>
-            <Button onClick={() => handleClick(false)} className='mb-4 h-8 bg-dark-blue text-primary-50'>
-              <FaArrowLeft className='mr-2 h-3 w-3' />
-              <p>Voltar</p>
-            </Button>
-            <p className='text-green-100 font-medium text-xl mb-4'>Ja recebemos seu pagamento, clique no botão abaixo para visualizar o voucher:</p>
+            <p className='text-green-100 font-medium text-xl mb-4'>Já recebemos seu pagamento, clique no botão abaixo para visualizar o voucher:</p>
             <Button className='bg-positive-green h-14 text-xl w-full' onClick={() => window.open(payment_success_url)}>Visualizar voucher</Button>
           </div>) : (
           <div className='flex flex-col gap-6'>
+            <div className='w-12'><DeleteVoucherCookieBtn label='Voltar' refresh={true} message='Se você já pagou e ainda não está vendo o botão para visualizar o voucher, atualize a pagina e tente novamente.' /></div>
             <p className='text-primary-100 font-medium'>Voucher criado com sucesso! Guarde o codigo abaixo, finalize o pagamento clicando no botão abaixo e volte ao site para utiliza-lo:</p>
             <h2 className='text-7xl font-bold text-center text-primary-50'>{code}</h2>
             <Link href={init_point} className='bg-positive-green rounded-xl text-center h-14 text-xl w-full flex justify-center items-center font-medium text-primary-50' onClick={redirectToPayment}>
