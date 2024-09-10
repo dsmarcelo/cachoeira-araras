15d44411bcba872b11353b3afc48f5a3025150d2
dsmarcelo <71904026+dsmarcelo@users.noreply.github.com>
2024-09-09 23:07:41 -0300

    feat: create show voucher card pagein /voucher

---
+++ src/app/(client)/voucher/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { api } from "@/trpc/server";
+import PaymentCard from '@/app/_components/payment-card';
+import VoucherCard from '@/app/_components/voucher-card';
+import { type PaymentResponse } from 'mercadopago/dist/clients/payment/commonTypes';
+import { type PreferenceResponse } from 'mercadopago/dist/clients/preference/commonTypes';
+import DeleteVoucherCookieBtn from '@/app/_components/delete-voucher-cookie-btn';
+
+const fetchPreference = async (preference_id: string): Promise<PreferenceResponse | null> => {
+  try {
+    const res = await api.mercadopago.getPreference({ preference_id });
+    if (!res) return null
+    return res;
+  } catch (error) {
+    console.error('Error fetching payment:', error);
+    throw error;
+  }
+};
+
+const fetchPayment = async (payment_id: string): Promise<PaymentResponse | null> => {
+  try {
+    const res = await api.mercadopago.getPayment({ payment_id });
+    if (!res) return null
+    return res;
+  } catch (error) {
+    console.error('Error fetching payment:', error);
+    throw error;
+  }
+};
+
+export default async function voucherPage({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>
+}) {
+  const allStrings = Object.values(searchParams).every(value => typeof value === 'string');
+
+  if (!allStrings) {
+    return <div className='text-center h-screen text-3xl'>Link inválido</div>
+  }
+
+  const { code, pid } = searchParams;
+  if (!code || !pid) return <div className='text-center h-screen text-3xl'>Link inválido</div>
+
+  const payment = await fetchPayment(pid as string)
+  if (!payment) return <div className='text-center h-screen text-3xl'>Erro ao buscar pagamento</div>
+
+  if (payment.status === 'denied') {
+    return <div>Pagamento não aprovado</div>
+  }
+
+  const voucher = await api.voucher.findByCode({ code: code as string });
+
+  if (!voucher) return <div className='text-center h-screen text-3xl'>Não foi possível encontrar o voucher</div>
+
+  const preference = await fetchPreference(voucher.preference_id);
+
+  return (
+    <div className="flex flex-col w-full pt-8 px-4 items-center pb-24 bg-bg-blue overflow-hidden">
+      <h1 className='text-center text-2xl font-bold text-primary-100 mb-8'>Obrigado por comprar seu voucher na Cachoeira das Araras!</h1>
+      <div className='w-full max-w-lg flex flex-col items-center gap-8'>
+        {preference && <PaymentCard data={preference} payment_id={pid as string} />}
+        <VoucherCard data={voucher} />
+        <DeleteVoucherCookieBtn />
+      </div>
+    </div>
+  )
+}
